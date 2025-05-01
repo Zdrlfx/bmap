a78ff64bccb7a33cb15ba2f837531bc633b0fe3e
Sweta <swetasuwal001@gmail.com>
2025-05-01 18:44:51 +0545

    oh yeah entity extraction successful using dependency parsing'

--- rasa_bot/actions/actions.py
+++ rasa_bot/actions/actions.py
@@ -1,27 +1,30 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
+import spacy
 
+# Load spaCy model
+nlp = spacy.load("en_core_web_sm")
 
-# This is a simple example for a custom action which utters "Hello World!"
+class ActionExtractLocations(Action):
+    def name(self):
+        return "action_extract_locations"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: dict):
+
+        text = tracker.latest_message.get("text", "")
+        doc = nlp(text)
+
+        origin = None
+        destination = None
+
+        for token in doc:
+            if token.dep_ == "pobj" and token.head.text.lower() == "from":
+                origin = token.text
+            elif token.dep_ == "pobj" and token.head.text.lower() == "to":
+                destination = token.text
+
+        dispatcher.utter_message(text=f"Origin: {origin}, Destination: {destination}")
+        return [SlotSet("origin", origin), SlotSet("destination", destination)]
